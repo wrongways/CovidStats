2b7e53df685905c8a34df4fb1b3c1db2210030fa
wrongways <jez@wain.name>
2020-08-26 12:26:22 -0600

    Initial Commit

---
+++ NetworkingSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NetworkingSwiftUI
+//
+//  Created by jez wain on 8/26/20.
+//  Copyright © 2020 jez wain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
